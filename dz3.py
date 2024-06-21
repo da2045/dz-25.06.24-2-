ce0edd3663f15e91350ea5b10f2b56e78e2c23ae
da2045 <vitde2009@gmail.com>
2024-06-22 02:05:48 +0300

    Add files via upload

---
+++ dz3.py
@@ -0,0 +1,62 @@
+# №1
+# day = int(input("enter your day -->"))
+# match day:
+#     case 1:
+#         print("mondey")
+#     case 2:
+#         print("tuesday")
+#     case 3:
+#         print("Wednesday")
+#     case 4:
+#         print("Thursday")
+#     case 5:
+#         print("Friday")
+#     case 6:
+#         print("Saturday")
+#     case 7:
+#         print("Sunday")  
+
+# №2
+# month = int(input("enter your mounth --> "))
+# match month:
+#     case 1:
+#         print("January")
+#     case 2:
+#         print("Fabruary")
+#     case 3:
+#         print("March")
+#     case 4:
+#         print("April")
+#     case 5:
+#         print("May")
+#     case 6:
+#         print("June")
+#     case 7:
+#         print("July")
+#     case 8:
+#         print("August")
+#     case 9:
+#         print("September")
+#     case 10:
+#         print("October")
+#     case 11:
+#         print("November")
+#     case 12:
+#         print("December")
+
+# №3 
+# num = int(input("Enter your num -->"))
+# if (num>0):
+#  print("Number is positive")
+# elif (num<0):
+#  print("Number is negative")
+# elif (num==0):
+#  print("Number is equal to zero")
+
+#№4
+# num1 = int(input("Enter your first num -->"))
+# num2 = int(input("Enter your second num -->"))
+# if (num1==num2):
+#     print("num1 = num2")
+# else:
+#     print(max(num1, num2), min(num1, num2))
